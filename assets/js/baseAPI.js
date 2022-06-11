51d300bc59b23ec83b0bf069384b58e9ac3f2f36
songhao <1146987523@qq.com>
2022-06-11 09:46:37 +0800

    完成了首页文件

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -10,4 +10,14 @@ $.ajaxPrefilter((option) => {
       Authorization: localStorage.getItem("token"),
     };
   }
+  //权限校验
+  option.complete = (res) => {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if(res.responseJSON.status ===1 && res.responseJSON.message === "身份认证失败！") {
+        //  强制清空 token
+        localStorage.removeItem("token");
+        // 强制跳转到登录页面
+        location.href = "/login.html"
+    }
+}
 });
